f5df6079fbb8ffa4f4caa7c2da420360a1993bf2
Dmitriy Prokoshev <pda1990@mail.ru>
2021-12-23 21:34:53 +0300

    Homework 8: class Player, class Game

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-import { player1, player2 } from './player.js';
-import { $arenas, $formFight } from './elements.js';
-import generateLogs from './functions/generate-log.js';
-import showResult from './functions/show-result.js';
-import createPlayer from './functions/create-player.js';
-import playerAttack from './functions/player-attack.js';
-import enemyAttack from './functions/enemy-attack.js';
+import Game from './game.js';
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+const game = new Game();
 
-generateLogs('start', player1, player2);
-
-$formFight.addEventListener('submit', function (e) {
-  e.preventDefault();
-  player1.currentAttack = playerAttack();
-  player2.currentAttack = enemyAttack();
-
-  player1.attack(player2);
-  player2.attack(player1);
-
-  showResult();
-});
+game.start();
